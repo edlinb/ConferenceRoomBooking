69c031398e125bbac2ced58adf5d744d0b11e515
Mateo Cakoni <cakoniteo@gmail.com>
2024-04-06 10:57:11 +0200

    shtuam modelet

---
+++ Models/ConferenceRooms.cs
@@ -0,0 +1,9 @@
+﻿namespace ConferenceRoomBooking.Models
+{
+    public class ConferenceRooms
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int MaximumCapacity { get; set; }
+    }
+}
